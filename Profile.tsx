887847eaed1a022a56537367d3a293909186cf19
lordkeh111-cyber <lord.keh111@gmail.com>
2026-02-04 19:25:30 -0300

    Update Profile.tsx

--- Profile.tsx
+++ Profile.tsx
@@ -5,3 +5,34 @@ const getGreeting = () => {
   if (hour < 18) return "Boa tarde 🌤️";
   return "Boa noite 🌙";
 };
+import { useState } from "react";
+
+export default function Profile() {
+  const [photo, setPhoto] = useState(
+    localStorage.getItem("photo")
+  );
+
+  const handlePhoto = (e: any) => {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+
+    reader.onload = () => {
+      localStorage.setItem("photo", reader.result as string);
+      setPhoto(reader.result as string);
+    };
+
+    reader.readAsDataURL(file);
+  };
+
+  return (
+    <>
+      <img
+        src={photo || "/avatar.png"}
+        width={80}
+        style={{ borderRadius: "50%" }}
+      />
+
+      <input type="file" onChange={handlePhoto} />
+    </>
+  );
+}
